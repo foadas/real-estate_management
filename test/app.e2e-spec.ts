78054483d0d39426d2fedfda7b3c492335659c37
foadas <foad1381@gmail.com>
2023-07-15 15:53:45 -0400

    first step of adding e2e testing

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,19 @@
+import { AppModule } from '../src/app.module';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+
+describe('App e2e', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+  });
+  afterAll(() => {
+    app.close();
+  });
+  it.todo('pass');
+});
